0e5d5488de235fd2717a14c943968daf24adf1ed
FLine <59862399+Yar666@users.noreply.github.com>
2022-06-24 16:52:36 +0300

    Delete gloshop directory

--- gloshop/keyboard/callback_data.py
+++
@@ -1,6 +0,0 @@
-from aiogram.utils.callback_data import CallbackData
-
-buy_callback = CallbackData("buy","item_name","quantity","cost")
-remove_callback = CallbackData("remove","autonum")
-pay_callback = CallbackData("pay","autonum","item_name")
-send_True = CallbackData("send","autonum")
--- gloshop/states.py
+++
@@ -1,9 +0,0 @@
-
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-
-class Purchase(StatesGroup):
-    Autonum=0
-    EnterQuantity = State()
-    Approval = State()
-    Payment = State()
